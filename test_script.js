222a7ddf6ba84c3cdaf71bf360b1739e144e2c67
Aileen Pang <aileenpang63@gmail.com>
2017-09-26 17:12:01 +0000

    returned output 1 with initial code

---
+++ test_script.js
@@ -0,0 +1,29 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0].number); //output: 1
+    client.end();
+  });
+});
+
+// What other information does the result object returned from query contain?
+// Why does the query function take in an array of arguments?
+// Is there any resemblance to the mongo driver that you used in Week 3, in this activity?
+// What is this new keyword that you see being used to create the client? You may not have seen this before.
