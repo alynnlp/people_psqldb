a719c1334796bbe708bd5115189d0a331760fdcc
Aileen Pang <aileenpang63@gmail.com>
2017-09-27 09:38:59 -0400

    cleaned up code

--- knexlookup_people.js
+++ knexlookup_people.js
@@ -1,13 +1,6 @@
-// const pg = require("pg");
+
 const settings = require("./settings"); // settings.json
-// const client = new pg.Client({
-//   user     : settings.user,
-//   password : settings.password,
-//   database : settings.database,
-//   host     : settings.hostname,
-//   port     : settings.port,
-//   ssl      : settings.ssl
-// });
+
 const knex = require("knex")({
   client: 'pg',
   connection: {
